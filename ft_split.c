b478eedc0a538c3021e34227c8fb93bd71ae99a8
ywake <ywake@student.42tokyo.jp>
2020-06-27 23:32:51 +0900

    add ft_split

---
+++ ft_split.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include "test.h"
+// #include "../Libft/libft.h"
+#include "../Libft/ft_split.c"
+
+void	test(char const *s, char c)
+{
+	printf("> src:\"%s\" c:'%c'\n", s, c);
+	char	**rtn = ft_split(s, c);
+	// int dif = ft_strncmp(expect, rtn, ft_strlen(expect + 1));
+	// dif == 0 ? printf(GREEN) : printf(BOLDRED);
+	// printf("    expect: \"%s\"\n", expect);
+	int i = 0;
+	while (rtn[i] != NULL)
+	{
+		printf("\"%s\"\n", rtn[i]);
+		free(rtn[i]);
+		i++;
+	}
+	free(rtn);
+	// printf(RESET);
+}
+
+int		main(void)
+{
+	printf("== basic input ==\n");
+	test("this is test", ' ');
+	test(" this is test ", ' ');
+	test("   this    is   test", ' ');
+
+	printf("\n== advanced input ==\n");
+	test("   ", ' ');
+	test("split  ||this|for|me|||||!|", '|');
+	test("      split       this for   me  !       ", ' ');
+	// test("  \t\t \t\t    \t\t", " \t", "");
+	// test("this is test", 5, 20);
+	// test("this is test", 20, 5);
+	// test("", 5, 3);
+	// // test(NULL, 5, 3); -> seg fault
+	// test("\200\t\x41\0", 0, 4);
+
+	printf("\n↓leakcheck\n\n");
+	while(1);
+}
