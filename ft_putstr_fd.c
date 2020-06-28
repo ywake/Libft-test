348e0ee09c5a2e35be194b69053c495657c8de3f
ywake <ywake@student.42tokyo.jp>
2020-06-28 13:38:42 +0900

    add ft_putstr_fd.c

---
+++ ft_putstr_fd.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include "test.h"
+#include "../Libft/libft.h"
+// #include "../Libft/ft_putstr_fd.c"
+
+void	test(char *s, int fd)
+{
+	printf("> \"%s\"\n", s);
+	ft_putstr_fd(s, fd);
+	// int dif = ft_strncmp(expect, rtn, ft_strlen(expect + 1));
+	// dif == 0 ? printf(GREEN) : printf(BOLDRED);
+	// printf("    expect: \"%s\"\n", expect);
+	// printf("ft_putchar_fd: \"%s\"\n", rtn);
+	// printf(RESET);
+}
+
+int		main(void)
+{
+	printf("== basic input ==\n");
+	test("this is test", 1);
+	// test('\n', 1);
+	// int fd = open("./out_ft_puchar_fd", O_RDWR);
+	// test('A', fd);
+	// close(fd);
+
+	// printf("\n== advanced input ==\n");
+	// test("this is test", 5, 20);
+	// test("this is test", 20, 5);
+	// test("", 5, 3);
+	// // test(NULL, 5, 3); -> seg fault
+	// test("\200\t\x41\0", 0, 4);
+
+	printf("\n↓leakcheck\n\n");
+	while(1);
+}
