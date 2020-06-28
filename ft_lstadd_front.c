86a5b31c72973ce3bbe5af87c4a3a1168912ebb0
ywake <ywake@student.42tokyo.jp>
2020-06-28 17:57:04 +0900

    ft_lstadd_front.c [c,n,t,l OK]

---
+++ ft_lstadd_front.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include "test.h"
+#include "../Libft/libft.h"
+
+void test(t_list **head, t_list *new, char *expect)
+{
+	t_list *old_head;
+
+	old_head = *head;
+	printf("head: %p\n", *head);
+	printf("head->content: %s\n", (*head)->content);
+	printf("head->next   : %p\n", (*head)->next);
+	printf("new: %p\n", new);
+	printf("new->content: %s\n", new->content);
+	printf("new->next   : %p\n", new->next);
+
+	printf("↓\nft_lstaddfront(head, new)\n↓\n");
+	ft_lstadd_front(head, new);
+	if (ft_strncmp((*head)->content, expect, strlen(expect)) == 0 && (*head)->next == old_head)
+		printf(GREEN);
+	else
+		printf(BOLDRED);
+	printf("head->content: %s\n", (*head)->content);
+	printf("head->next   : %p\n", (*head)->next);
+	printf("next->content: %s\n", (*head)->next->content);
+	printf("next->next   : %p\n", (*head)->next->next);
+	printf(RESET);
+	free((*head)->next);
+	free(*head);
+}
+
+int main(void)
+{
+	t_list **head;
+	t_list *new;
+	head = (t_list **)ft_calloc(1, sizeof(t_list *));
+	*head = ft_lstnew("first lst");
+	new = ft_lstnew("second lst");
+	test(head, new, "second lst");
+
+	printf("\n↓leakcheck\n\n");
+	while(1);
+}
