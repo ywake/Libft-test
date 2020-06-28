1940e567a60a823488941a19cde7c1ebfee8ceea
ywake <ywake@student.42tokyo.jp>
2020-06-28 18:47:10 +0900

    add ft_lstlast.c

---
+++ ft_lstlast.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include "test.h"
+#include "../Libft/libft.h"
+
+void test(t_list *lst, t_list *expect)
+{
+	t_list *rtn = ft_lstlast(lst);
+
+	rtn == expect ? printf(GREEN) : printf(BOLDRED);
+	printf("expect: %p\n", expect);
+	printf("rtn   : %p\n", rtn);
+	printf(RESET);
+}
+
+int main(void)
+{
+	t_list **head;
+	t_list *new;
+	head = (t_list **)ft_calloc(1, sizeof(t_list *));
+	*head = ft_lstnew("first lst");
+	new = ft_lstnew("second lst");
+	ft_lstadd_front(head, new);
+
+	printf("== usual intput ==\n");
+	test(*head, new->next);
+
+	printf("== unusual intput ==\n");
+	printf("> NULL input\n");
+	test(NULL, NULL);
+
+	free((*head)->next);
+	free(*head);
+
+	printf("\n↓leakcheck\n\n");
+	while(1);
+}
